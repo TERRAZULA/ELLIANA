d74713913b3c3a94b8a227e4d7e589b9b32811b6
TERRAZULA <152370621+TERRAZULA@users.noreply.github.com>
2023-12-09 19:17:55 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def get_img_as_base64(file):
         return base64.b64encode(data).decode()
 
 
-img = get_img_as_base64("snsu.png")
+img = get_img_as_base64("back.jpg")
 page_bg_img = f"""
     <style>
     [data-testid="stAppViewContainer"] > .main {{
